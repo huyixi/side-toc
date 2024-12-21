2cf4d53e88403e9ddd62bcd4e313e24968d48f20
huyixi <hi@huyixi.com>
2024-12-21 19:42:13 +0800

    feat: get tabs update

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,9 @@
-chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch((error) => console.error(error));
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete") {
+    console.log("tabs updated", tabId, changeInfo, tab);
+  }
+});
