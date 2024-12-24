1c517db217ef803b797fad70cf6cee5c08c4bca4
huyixi <hi@huyixi.com>
2024-12-24 15:02:21 +0800

    chore: delete options template

--- src/background.ts
+++ src/background.ts
@@ -2,8 +2,11 @@ chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error));
 
+function updateSidePanel() {}
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete") {
     console.log("tabs updated", tabId, changeInfo, tab);
+    updateSidePanel();
   }
 });
